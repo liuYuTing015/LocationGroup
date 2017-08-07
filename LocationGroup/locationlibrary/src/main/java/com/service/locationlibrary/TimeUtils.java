840c7613fb099de6a19919c6ee01b24b6f0de1b7
liuYuTing <053525>
2017-08-07 17:26:01 +0800

    add:UTC 时区

--- LocationGroup/locationlibrary/src/main/java/com/service/locationlibrary/TimeUtils.java
+++ LocationGroup/locationlibrary/src/main/java/com/service/locationlibrary/TimeUtils.java
@@ -3,13 +3,16 @@ package com.service.locationlibrary;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
-
 /**
  * Created by Ting on 17/7/4.
  */
 
 public class TimeUtils {
     public final static SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    static {
+        TIME_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
 
     public static String parseDate(long time) {
         Date date = new Date(time);
